8d268a1c2050aa9f9f6e3681f416c3c6d6f94a30
tegonmatheus <tegonmatheus@gmail.com>
2022-03-24 17:52:06 -0300

    att

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import axios from 'axios'
-axios.defaults.baseURL = 'http://lanchedb.azurewebsites.net'
+axios.defaults.baseURL = 'https://lanchedb.azurewebsites.net'
 //process.env.VUE_APP_BASE_URL
 Vue.use({
   install (Vue) {
