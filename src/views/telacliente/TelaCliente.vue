d6472f0e6ebd531cfad482c1a8630a06db2256f1
tegonmatheus <tegonmatheus@gmail.com>
2022-04-12 22:15:50 -0300

    att

--- src/views/telacliente/TelaCliente.vue
+++ src/views/telacliente/TelaCliente.vue
@@ -586,6 +586,7 @@ export default {
   },
 
   methods: {
+    
 
     callback_dialog() {
       this.dialogOptions.dialog = false;
@@ -597,7 +598,10 @@ export default {
         return;
       }
       
-        if( /iPhone|iPad|iPod/i.test(navigator.userAgent) )
+        if(  navigator.userAgent.match(/iPhone/i)
+          || navigator.userAgent.match(/iPad/i)
+          || navigator.userAgent.match(/iPod/i) 
+          )
         {
           if (this.cliente.troco > 0) {
             
